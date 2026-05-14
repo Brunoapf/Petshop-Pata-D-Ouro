e0daff7f0fa0874ed6e49795286d6ab714152d47
Bruno <brunoaraujopf@gmail.com>
2026-05-14 18:33:22 -0300

    Commit Incial Do Projeto

---
+++ core/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+from django.db import models
+
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=20)
+    email = models.EmailField()
+    endereco = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nome
+
+
+class Pet(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+
+    nome = models.CharField(max_length=100)
+    especie = models.CharField(max_length=50)
+    raca = models.CharField(max_length=50)
+    idade = models.IntegerField()
+    porte = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.nome
+
+
+class Servico(models.Model):
+    nome = models.CharField(max_length=100)
+    preco = models.DecimalField(max_digits=8, decimal_places=2)
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.nome
+
+
+class Agendamento(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+    servico = models.ForeignKey(Servico, on_delete=models.CASCADE)
+
+    data = models.DateField()
+    horario = models.TimeField()
+
+    status = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.pet} - {self.data}"
